4497842b4b62fe6d60e251c86c640f0e64ef3800
wendryuslima <wendryuslima31@gmail.com>
2026-07-16 21:47:14 -0300

    feat: add logging functionality and improve user creation process

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,20 +1,40 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wendryuslima/user-manager/src/configuration/logger"
 	"github.com/wendryuslima/user-manager/src/configuration/validation"
 	"github.com/wendryuslima/user-manager/src/controller/model/request"
+	"github.com/wendryuslima/user-manager/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init Create controller", 
+	zap.String("journey", "createUser"),
+)
 	var userRequest request.UserRequest
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err, zap.String("journey", "createUser"), )
 		restError := validation.ValidateUserError(err)
 	c.JSON(int(restError.Code), restError)
 	return
 	}
 
-	fmt.Println(userRequest)
+	
+
+	response := response.UserResponse{
+		
+		ID: "test",
+		Email: userRequest.Email,
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+
+	}
+
+	logger.Info("User create succesfully", 
+	zap.String("journey", "createUser"))
+	c.JSON(http.StatusOK, response)
 }
